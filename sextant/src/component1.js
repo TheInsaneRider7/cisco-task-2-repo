b9a85e2f6797b47580966fb2fc23507fff3eaf2b
TheInsaneRider7 <109857894+TheInsaneRider7@users.noreply.github.com>
2022-07-26 02:46:21 +0400

    Add files via upload

---
+++ sextant/src/component1.js
@@ -0,0 +1,10 @@
+import React from 'react' 
+import './component1.css'
+const Component1 = ()=>{
+    return(
+    <div className="component1" id="component1">
+    <h1>Component1</h1>
+    </div>
+    )
+}
+export default Component1
